7d0b46a928b60c3356690a0079a741c8a8793d10
lynnvdhoornn <lynnschonk@gmail.com>
2024-10-12 19:28:46 +0200

    upgrade

--- javascript.js
+++ javascript.js
@@ -13,19 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // titles floating
 document.addEventListener("DOMContentLoaded", function() {
-    // Selecteer h2's (met uitzondering van die in de 4e sectie), afbeeldingen, div's in de 2e en 4e sectie
     const elements = document.querySelectorAll('header h1, header img, h2, header p, section ul:first-of-type, h3, p:not(footer), form');
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('in-view');
-                observer.unobserve(entry.target); // Stop met observeren zodra het element zichtbaar is
+                observer.unobserve(entry.target);
             }
         });
     }, { threshold: 0.5 });
 
-    // Observeer alle geselecteerde elementen (h2's, afbeeldingen, div's in de 2e en 4e sectie)
     elements.forEach(element => {
         observer.observe(element);
     });
--- script.js
+++ script.js
@@ -13,21 +13,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // image rotation
-const rotatingImage = document.querySelector('header img:first-of-type');
+const rotatingImages = document.querySelectorAll('header img:first-of-type, section:nth-of-type(5) img');
 
 function updateRotation() {
     const scrollPosition = window.scrollY;
     const rotationAngle = scrollPosition * 0.2;
-    rotatingImage.style.transform = `rotate(${rotationAngle}deg)`;
+
+    rotatingImages.forEach(image => {
+        image.style.transform = `rotate(${rotationAngle}deg)`;
+    });
 }
 
 window.addEventListener('scroll', updateRotation);
 
 
 
+
 // floating list-items
 document.addEventListener("DOMContentLoaded", function() {
-    const skillItems = document.querySelectorAll('section:nth-of-type(3) li');
+    const skillItems = document.querySelectorAll('section:nth-of-type(4) li');
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -69,8 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // titles floating
 document.addEventListener("DOMContentLoaded", function() {
-    // Selecteer h2's (met uitzondering van die in de 4e sectie), afbeeldingen, div's in de 2e en 4e sectie
-    const elements = document.querySelectorAll('section h2:not(section:nth-of-type(4) h2), section img, section:nth-of-type(2) div, section:nth-of-type(4) div');
+    const elements = document.querySelectorAll('section h2:not(section:nth-of-type(3) h2), section img, section:nth-of-type(2) div, section:nth-of-type(5) div');
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -81,7 +84,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }, { threshold: 0.5 });
 
-    // Observeer alle geselecteerde elementen (h2's, afbeeldingen, div's in de 2e en 4e sectie)
     elements.forEach(element => {
         observer.observe(element);
     });
@@ -91,16 +93,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // moving text while scrolling
-const uxUiSpan = document.querySelector('section:nth-of-type(3) h2 span:first-child');
-const skillsSpan = document.querySelector('section:nth-of-type(3) h2 span:last-child');
+const scrollRight = document.querySelector('section:nth-of-type(3) span:first-of-type');
+const scrollLeft = document.querySelector('section:nth-of-type(3) span:last-of-type');
 
 window.addEventListener('scroll', () => {
-    const section = document.querySelector('section:nth-of-type(3)');
-    const sectionPosition = section.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+    const scrollAmount = window.scrollY;
 
-    const scrollPercentage = Math.min(Math.max(0, (windowHeight - sectionPosition) / windowHeight), 1);
+    let rightTranslate = scrollAmount / 5 - 580; 
+    let leftTranslate = -scrollAmount / 5 + 500; 
 
-    uxUiSpan.style.transform = `translateX(${(-30 + scrollPercentage * 30)}%)`;
-    skillsSpan.style.transform = `translateX(${(30 - scrollPercentage * 30)}%)`;
+    scrollRight.style.transform = `translateX(${rightTranslate}px)`;
+    scrollLeft.style.transform = `translateX(${leftTranslate}px)`;
 });
+
